7a6e017a9f5077b09b37a13c1e91be794c76906c
Fadi Azzam <azzam@infai.org>
2022-09-12 12:28:19 +0200

    ADD: header design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,29 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from "react-router-dom";
+
 
 import SvgLogo from '../static/imgs/SvgLogo';
 import SvgLogoText from '../static/imgs/SvgLogoText';
 
-import SvgLogoAnimation from '../static/imgs/SvgLogoAnimation'
 import BubbleContainer from './BubbleContainer'
 const Header = () => {
+    const [bgColor, setBgColor] = useState("")
+    window.onscroll = function() {
+
+        if (window.pageYOffset > 400) {
+            setBgColor("bg-primary")
+        } else {
+            setBgColor("")
+        }
+    }
     return (
-        <header className="App-header">
-            <div className='container'>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-                    <div className="container-fluid">
+        <header className="App-header background">
+            <div className='container center-element'>
+                <nav className={`navbar navbar-expand-lg navbar-dark fixed-top ${bgColor}`}>
+                    <div className="container">
                         <a className="navbar-brand" href="/">
-                            <span className="me-2">
-                                <SvgLogo />
-                            </span>
-                            <SvgLogoText width="66" height="100%" />
+                            Drusen in Sachsen e. V.
                         </a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
@@ -41,6 +47,18 @@ const Header = () => {
                                     </ul>
                                 </li> */}
                                 <li className="nav-item">
+                                    <Link className='nav-link ' to="/contact">Über uns</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className='nav-link ' to="/contact">Über Drusen</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className='nav-link ' to="/contact">Nachrichten</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className='nav-link ' to="/contact">Registrieren</Link>
+                                </li>
+                                <li className="nav-item">
                                     <Link className='nav-link ' to="/contact">Contact</Link>
                                 </li>
                                 <li className="nav-item">
@@ -52,9 +70,16 @@ const Header = () => {
                     </div>
                 </nav>
                 <div className='row'>
-                    <div className="col-md-12 landingPageHeader">
-                        <div className='d-flex justify-content-center align-items-center h-100 w-100'>
-                            <SvgLogoAnimation width="100%" height="30%" className="w-6" />
+                    <div className="col-md-12 ">
+                        <div className='d-flex justify-content-center align-items-center h-100 w-100 text-light'>
+                            <div className="container">
+                                <h1 className="m-w30">Seien Sie heute einer von unseren drusischen Gemeinschaft!</h1>
+                                <p className="lead text-muted">Drusen in Sachsen e. V.</p>
+                                <p>
+                                    <a href="#" className="btn btn-primary me-2">Spenden</a>
+                                    <a href="#" className="btn btn-secondary">Registrieren</a>
+                                </p>
+                            </div>
                         </div>
                         <BubbleContainer />
                     </div>
